528d0a00879d2c1c5f6ede3bbc78803617218884
SSExpedicao <sessoes.expedicao@tc.df.gov.br>
2026-06-08 06:24:15 -0300

    expedicao.py

--- expedicao.py
+++ expedicao.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-from datetime import datetime, date, timedelta
+from datetime import dt
 import io
 import time
 import unicodedata
