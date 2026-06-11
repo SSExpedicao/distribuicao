760305f2233ddfd54829dfa0681966a037517e0b
SSExpedicao <sessoes.expedicao@tc.df.gov.br>
2026-06-11 04:07:16 -0300

    expedicao.py

--- expedicao.py
+++ expedicao.py
@@ -60,7 +60,8 @@ def higienizar_dados(processo, relator=""):
     rel_limpo = str(relator).strip().upper()
     mapa_relatores = {
         "RR": "GCRR", "AM": "GCAM", "PT": "GCPT", 
-        "AC": "GCAC", "IM": "GCIM", "MM": "GCMM", "VF": "GAVF", "GAVF / Subst.": "GAVF", "VF / Subst.": "GAVF"
+        "AC": "GCAC", "IM": "GCIM", "MM": "GCMM", "VF": "GAVF", 
+        "GAVF / Subst.": "GAVF", "VF / Subst.": "GAVF", "AT": "GPAT"
     }
     if rel_limpo in mapa_relatores:
         rel_limpo = mapa_relatores[rel_limpo]
